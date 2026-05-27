91ae0f03964dbb1a75b961e029bec0c461284c4b
bassem-krayem <bassem.krayem09@gmail.com>
2026-05-28 00:13:37 +0100

    feature: secured the api by adding the forgotPassword route which send an email to the user which containes the reset password token to the user then i added the route resetPassword route where the user can hit it with patch request  to reset his password i have added the node mailer package to allow sending emzils as well

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
 import bcrypt from 'bcryptjs';
+import crypto from 'crypto';
 import validator from 'validator';
 
 const userSchema = new mongoose.Schema(
@@ -102,6 +103,21 @@ userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
 
   // False means NOT changed
   return false;
+};
+
+userSchema.methods.createPasswordResetToken = function () {
+  const resetToken = crypto.randomBytes(32).toString('hex');
+
+  this.passwordResetToken = crypto
+    .createHash('sha256')
+    .update(resetToken)
+    .digest('hex');
+
+  // console.log({ resetToken }, this.passwordResetToken);
+
+  this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
+
+  return resetToken;
 };
 
 const User = mongoose.model('User', userSchema);
